35435926e5705430ba53f79d9fb57aa3e44907a9
devil1165 <devkant@getambee.com>
2018-10-25 15:14:22 +0530

    filter added to not to show selected cities in reading api response

--- api/routes/readings.js
+++ api/routes/readings.js
@@ -1,34 +1,56 @@
 const express = require('express');
 // const mongoose = require('mongoose');
 const checkAuth = require('../middleware/check-auth');
-
 const router = express.Router();
 const Reading = require('../models/readingModel')
-const NodeGeocoder = require('node-geocoder');
+// const NodeGeocoder = require('node-geocoder');
+const City = require('../models/citiesModel');
+
 
 
 // get all points
-router.get('/', checkAuth, (req,res,next) => { 
-    Reading.find().
-    limit(8).
-    select('location city country distance measurements geoLoc').exec()
-    .then(docs => {
-        const response = {
-            count:docs.length,
-            reading:docs,
-            request:{
-                type:'GET',
-                // url:'http://localhost:3000/products/'+
+router.get('/', checkAuth, (req,res,next) => {
+   var selectedCity = getAllSelectedCities('5bc5c37f96d69b20873a2cd0')
+   selectedCity.then(function(result) {
+        Reading.find({_id:{ $nin: result }}).
+        limit(8).
+        select('location city country distance measurements geoLoc').exec()
+        .then(docs => {
+            const response = {
+                count:docs.length,
+                reading:docs,
+                request:{
+                    type:'GET',
+                    // url:'http://localhost:3000/products/'+
+                }
             }
-        }
-        res.status(200).json(response)
+            res.status(200).json(response)
+        })
+        .catch(err => {
+            res.status(500).json({
+                error:err
+            })
+        })
+    
     })
-    .catch(err => {
+    .catch(err=>{
         res.status(500).json({
             error:err
         })
     })
 })
+
+function getAllSelectedCities (args){
+    return City.find({user:args}).then(result =>{
+        var arr = [];
+        result.map((v,i)=>{
+            arr.push(v.city)
+        })
+        return arr;
+    }).catch(err =>{
+        return err
+    })
+}
 
 // get points by lat:lng
 router.get('/:cityId', checkAuth, (req,res,next)=>{
