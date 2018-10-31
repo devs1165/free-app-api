7fd0a9ef6122b39549f2fa48141e05d771379c64
devil1165 <devkant@getambee.com>
2018-10-31 20:23:09 +0530

    connection string changed

--- api/routes/mapsdata.js
+++ api/routes/mapsdata.js
@@ -13,7 +13,7 @@ router.get('/', checkAuth, (req,res,next) => {
     Reading.find( { geoLoc: { $geoWithin: { $center: [ [lat, lng], radius ] } } })
     // .limit(parseInt(limit))
     .sort({ timestamp : -1})    
-    .select('location city country distance measurements geoLoc')
+    .select('location timestamp city country distance measurements geoLoc')
     .exec()
     .then(docs => {
         // console.log(docs)
